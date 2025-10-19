153b695b47a94c76f53e9b0ae1f916d67f14b8b8
MdRiabi <mdriabi@gmail.com>
2025-10-19 13:17:18 +0100

    the first static home controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        // Statistics with default values
+        $stats = [
+            'tasksCreated' => 15420,
+            'tasksCompleted' => 12856,
+            'activeUsers' => 3240,
+        ];
+
+        return $this->render('home/index.html.twig', [
+            'stats' => $stats,
+        ]);
+    }
+}
